16826b123926b431b9380b9aaf1c3ec934caca35
amitamitvaghamshi9@gmail.comamitgit config --global user.email amitvaghamshi9@gmail.com <amitvaghamshi9@gmail.comgit config --global user.name amit>
2023-05-01 00:02:31 +0530

    Done

--- MailApp/src/main/java/com/mail/controller/MessageController.java
+++ MailApp/src/main/java/com/mail/controller/MessageController.java
@@ -2,6 +2,8 @@ package com.mail.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +30,8 @@ public class MessageController {
 	
 	@Autowired
 	private MessageDetailsService messageService;
-
+	
+//	Logger logger = LoggerFactory.getLogger(MessageController.class);
 	
 	@PostMapping("/send")
 	public ResponseEntity<MessageDetails> sendMessage(@RequestParam("from") String from ,@RequestParam("to") String to,@RequestBody MessageDetails mail) throws UsersExceptions{
@@ -110,25 +113,12 @@ public class MessageController {
 	public ResponseEntity<Integer> getCountOfInboxMailHandler(@PathVariable("to") String to) throws  MessageDetailsException{
 		Integer  count=messageService.getCountOfMailInInbox(to);
 		
-	//	countofInbox ib=new countofInbox(count);
+	
+		logger.info("to has"+count+" in inbox");
 		
 		return new ResponseEntity<>(count,HttpStatus.OK);
 	}
-//	// inner class to send count
-//	class countofInbox{
-//		Integer count;
-//		countofInbox(Integer count){
-//			this.count=count;
-//		}
-//		
-//	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+
 	
 }
